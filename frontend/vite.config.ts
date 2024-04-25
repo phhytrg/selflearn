3d16c96a452cfb6c2db61d7a666a4f462074c874
phhytrg <truong.pht.7080@gmail.com>
2024-04-25 11:46:28 +0700

    chore: set up alias imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': `${path.resolve(__dirname, './src')}/shared/components`,
+      '@constants': `${path.resolve(__dirname, './src')}/shared/constants`,
+      '@apis': `${path.resolve(__dirname, './src')}/shared/apis`,
+      '@hooks': `${path.resolve(__dirname, './src')}/shared/hooks`,
+    },
+  },
 });
