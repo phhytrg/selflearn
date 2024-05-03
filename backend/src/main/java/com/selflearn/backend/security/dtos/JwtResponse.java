321a0fa75889bf18dd0a37dcde104bbbcca3f927
phhytrg <truong.pht.7080@gmail.com>
2024-05-03 16:40:41 +0700

    chore: use record for JwtResponse

--- backend/src/main/java/com/selflearn/backend/security/dtos/JwtResponse.java
+++ backend/src/main/java/com/selflearn/backend/security/dtos/JwtResponse.java
@@ -1,10 +1,4 @@
 package com.selflearn.backend.security.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class JwtResponse {
-    private String accessToken;
+public record JwtResponse(String accessToken) {
 }
