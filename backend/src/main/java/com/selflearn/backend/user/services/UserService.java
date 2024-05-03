6b5aeefff54243853042b719830d39833aac9b56
phhytrg <truong.pht.7080@gmail.com>
2024-05-03 14:57:27 +0700

    chore: inject through interface instead of class instance

---
+++ backend/src/main/java/com/selflearn/backend/user/services/UserService.java
@@ -0,0 +1,11 @@
+package com.selflearn.backend.user.services;
+
+import com.selflearn.backend.user.models.User;
+
+import java.util.UUID;
+
+public interface UserService {
+    User fetchUserById(UUID id);
+    User fetchUserByEmail(String email);
+    User saveUser(User user);
+}
