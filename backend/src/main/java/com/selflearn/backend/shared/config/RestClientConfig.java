0799165cc0fe43e510c609a3eaaa51eb4096a7fe
phhytrg <truong.pht.7080@gmail.com>
2024-05-06 14:12:29 +0700

    mend

--- backend/src/main/java/com/selflearn/backend/shared/config/RestClientConfig.java
+++ backend/src/main/java/com/selflearn/backend/shared/config/RestClientConfig.java
@@ -1,8 +1,10 @@
 package com.selflearn.backend.shared.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+@Configuration
 public class RestClientConfig {
     @Bean
     public RestClient restClient() {
