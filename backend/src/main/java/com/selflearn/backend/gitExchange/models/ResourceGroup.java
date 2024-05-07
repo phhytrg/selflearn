0990b19d1f2444228b54bd519d2a1f24c5751e01
phhytrg <truong.pht.7080@gmail.com>
2024-05-07 13:30:35 +0700

    feat: entites for git sample projects

--- backend/src/main/java/com/selflearn/backend/gitExchange/models/ResourceGroup.java
+++ backend/src/main/java/com/selflearn/backend/gitExchange/models/ResourceGroup.java
@@ -5,26 +5,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
-@Entity(name = "clusters")
-public class Cluster {
+@Entity(name = "resource_groups")
+public class ResourceGroup {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
-    private String provisioningState;
-    private String powerState;
-    private int nodeCount;
-    private String mode;
-    private String nodeImageVersion;
-    private String k8sVersion;
-    private String nodeSize;
-    private String os;
 
     @ManyToOne
-    private ResourceGroup resourceGroup;
+    private Subscription subscription;
+
+    @OneToMany
+    private List<Cluster> clusters;
 }
