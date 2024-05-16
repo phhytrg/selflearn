f57a8eb14543089a33bb9f81f95430406710679e
phhytrg <truong.pht.7080@gmail.com>
2024-05-16 14:48:17 +0700

    fix

--- backend/src/main/java/com/selflearn/backend/BackendApplication.java
+++ backend/src/main/java/com/selflearn/backend/BackendApplication.java
@@ -3,10 +3,11 @@ package com.selflearn.backend;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BackendApplication {
     @Bean
     public LocalValidatorFactoryBean validator() {
