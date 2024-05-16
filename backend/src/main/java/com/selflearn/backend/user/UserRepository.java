7d00f008b3586638322236e7281e6a43b62bda83
phhytrg <truong.pht.7080@gmail.com>
2024-05-16 14:45:21 +0700

    fix

--- backend/src/main/java/com/selflearn/backend/user/UserRepository.java
+++ backend/src/main/java/com/selflearn/backend/user/UserRepository.java
@@ -3,9 +3,11 @@ package com.selflearn.backend.user;
 import com.selflearn.backend.user.models.User;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     @EntityGraph(attributePaths = {"roles"})
     User findByEmail(String email);
